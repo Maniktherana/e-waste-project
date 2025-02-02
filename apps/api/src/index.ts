c8dfbc10b053d256c74c345c5ae6f87fd3512107
Manik Rana <manikrana54@gmail.com>
2025-02-02 18:54:21 +0530

    feat: add gemini updates

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,37 @@
 import { Hono } from "hono";
+import { streamSSE } from "hono/streaming";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/submit", async (c) => {
+  return streamSSE(c, async (stream) => {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      systemInstruction: "",
+    });
+
+    const prompt = "Explain how AI works";
+
+    const result = await model.generateContentStream(prompt);
+    let i = 0;
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      console.log(chunkText);
+      await stream.writeSSE({
+        data: chunkText,
+        event: "gemini-response",
+        id: i.toString(),
+      });
+      i++;
+    }
+  });
+});
+
+app.get("/health", (c) => {
+  return c.json({ status: "healthy" });
 });
 
 export default app;
