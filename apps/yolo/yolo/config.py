21334927aa92a42fc5a0d57a9d100207991d5f9d
Manik Rana <manikrana54@gmail.com>
2025-03-19 13:22:27 +0530

    feat: new weights

--- apps/yolo/yolo/config.py
+++ apps/yolo/yolo/config.py
@@ -2,7 +2,7 @@ import os
 from typing import Dict, Any
 
 SETTINGS: Dict[str, Any] = {
-    "model_path": os.path.join(os.getcwd(), "weights.pt"),
+    "model_path": os.path.join(os.getcwd(), "new.pt"),
     "detection_confidence": 0.25,
     "detection_interval": 5,
     "log_interval": 1.0,
